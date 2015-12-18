dd1a48f7ec615633e73b984ddc0d0b4fc39eff58
lmo0 <luis.mayorga@nreca.coop>
2015-12-17 19:25:43 -0500

    add silent to wget

--- scripts/bootstrap_master.sh
+++ scripts/bootstrap_master.sh
@@ -1,3 +1,17 @@
 
-rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-6.noarch.rpm
-yum install puppetserver -y
+#CHECK IF TARBALL EXISTS
+
+#IF DOES NOT EXIST THEN DOWNLOAD
+#ELSE CONTINUE
+
+# CHECK IF IS ALREADY INSTALLED
+# IF NOT INSTALL
+# ELSE PRINT MESSAGE
+
+
+wget --quiet https://s3.amazonaws.com/pe-builds/released/2015.3.0/puppet-enterprise-2015.3.0-el-6-x86_64.tar.gz
+
+
+
+#rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-6.noarch.rpm
+#yum install puppetserver -y
