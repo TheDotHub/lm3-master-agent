dbf5e82bb084b10326bce583b506014f7fcce093
Diego Augusto Otoya Paz <Diego Augusto Otoya Paz>
2015-12-19 05:11:25 -0500

    updated

--- scripts/bootstrap_master.sh
+++ scripts/bootstrap_master.sh
@@ -4,12 +4,24 @@
 #IF DOES NOT EXIST THEN DOWNLOAD
 #ELSE CONTINUE
 
+if [ ! -f "/home/vagrant/puppet-enterprise-2015.3.0-el-6-x86_64.tar.gz" ]; then
+  wget https://s3.amazonaws.com/pe-builds/released/2015.3.0/puppet-enterprise-2015.3.0-el-6-x86_64.tar.gz
+else echo "Already downloaded"
+fi
+
 # CHECK IF IS ALREADY INSTALLED
 # IF NOT INSTALL
 # ELSE PRINT MESSAGE
 
+# 
+if ! dpkg -l "puppet-agent" &> /dev/null ; then
+  echo "Installing puppet master"
+  sudo tar -zxvf /home/vagrant/puppet-enterprise-2015.3.0-el-6-x86_64.tar.gz
+  cd /home/vagrant/puppet-enterprise-2015.3.0-el-6-x86_64 && ./puppet-enterprise-installer -a /vagrant/scripts/master.server.answers
+else echo "Already installed"
+fi
 
-wget --quiet https://s3.amazonaws.com/pe-builds/released/2015.3.0/puppet-enterprise-2015.3.0-el-6-x86_64.tar.gz
+#wget --quiet https://s3.amazonaws.com/pe-builds/released/2015.3.0/puppet-enterprise-2015.3.0-el-6-x86_64.tar.gz
 
 
 #rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-6.noarch.rpm
